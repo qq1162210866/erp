691c6c2df1170ab39090ff799fcf872b762dcdf7
1162210866 <1162210866@qq.com>
2019-05-07 15:58:02 +0800

    初始化提交
    提交了代码，后面会有改动

---
+++ src/main/java/com/jsh/erp/config/MyConfiguration.java
@@ -0,0 +1,31 @@
+package com.jsh.erp.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * MyConfiguration.java
+ * Description: 解决跨域的配置文件
+ *
+ * @author Peng Shiquan
+ * @date 2019-05-07
+ */
+@Configuration
+public class MyConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
